39169587472f55b56e35e6a69d2c472ca599eafe
nonnonno23456 <oakee92@gmail.com>
2025-11-10 02:49:49 +0900

    Tower_Defens#02

---
+++ Assets/scripts/Rpojectile.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Movement2D movement2D;
+    private Transform target;
+
+    public void Setup(Transform target)
+    {
+        movement2D = GetComponent<Movement2D>();
+        this.target = target; // 타워가 설정해준 target
+    }
+
+    private void Update()
+    {
+        if (target != null) // target이 존재하면
+        {
+            // 발사체를 target의 위치로 이동
+            Vector3 direction = (target.position - transform.position).normalized;
+            movement2D.MoveTo(direction);
+        }
+        else // 여러 이유로 target이 사라지면
+        {
+            // 발사체 오브젝트 삭제
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Enemy")) return; // 적이 아닌 대상과 부딪히면
+        if (collision.transform != target) return;  // 현재 target인 적이 아닐 때
+
+        collision.GetComponent<Enemy>().OnDie(); // 적 사망 함수 호출
+        Destroy(gameObject);                     // 발사체 오브젝트 삭제
+    }
+}
+
+/*
+ * File : Projectile.cs
+ * Desc : 타워가 발사하는 기본 발사체에 부착
+ * Functions
+ * : Update() - 타겟이 존재하면 타겟 방향으로 이동하고, 타겟이 존재하지 않으면 발사체 삭제
+ * : OnTriggerEnter2D() - 타겟으로 설정된 적과 부딪혔을 때 둘 다 삭제
+ */
---
+++ Assets/scripts/Tile.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    // 타일에 타워가 건설되어 있는지 검사하는 변수
+    public bool IsBuildTower { set; get; }
+
+    private void Awake()
+    {
+        IsBuildTower = false;
+    }
+}
+
+/*
+ * File : Tile.cs
+ * Desc : 타워 배치가 가능한 TileWall 오브젝트에 부착
+ */
